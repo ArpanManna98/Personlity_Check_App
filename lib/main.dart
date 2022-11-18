8f7d8b744b9ce0e6557a34dece0aaf6efce480c5
ArpanManna98 <arpanmanna98@gmail.com>
2022-11-18 21:58:44 +0530

    buttons added without interaction

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print("Answer Choosen!");
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite colour?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: [
+            Text('The Questions !'),
+            ElevatedButton(
+              child: Text('Answer 1'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 2'),
+              onPressed: answerQuestion,
+            ),
+            ElevatedButton(
+              child: Text('Answer 3'),
+              onPressed: answerQuestion,
+            ),
+          ],
+        ),
       ),
     );
   }
